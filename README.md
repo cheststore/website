1b420da862a7bc15de2143b27558c3ef328dbe59
Lance Whatley <whatl3y@gmail.com>
2020-06-06 14:20:30 -0400

    Update landing page, make form submit async, add git example

--- README.md
+++ README.md
@@ -7,13 +7,13 @@ https://github.com/BlackrockDigital/startbootstrap-grayscale
 ## Deployment
 
 chest.store's website is deployed to AWS S3 as a static site. Use the
-following instructions to deploy after changes have been make.
+following instructions to deploy after changes are made.
 
 ### macOS
 
 1. Make sure AWS CLI is installed via [Homebrew](https://brew.sh/)
     - `brew install awscli`
-2. After changes are made, build to `dist`
+2. After changes are made, build files to `dist` directory
     - `npm run build`
 3. `aws s3 cp --recursive dist s3://chest.store`
 4. Clear cloudfront distribution cache (via UI today, TODO instructions via CLI)
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -6,6 +6,9 @@
     (function ($) {
     "use strict"; // Start of use strict
 
+    // init highlight.js
+    hljs.initHighlightingOnLoad();
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
@@ -54,3 +57,50 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+// form submittal
+(function () {
+    window.addEventListener("DOMContentLoaded", function () {
+        toastr.options.positionClass = "toast-bottom-right";
+
+        // get the form elements defined in your form HTML above
+
+        var form = document.getElementById("signup-form");
+
+        // Success and Error functions for after the form is submitted
+
+        function success() {
+            form.reset();
+            toastr.success(`Successfully signed you up!`);
+        }
+
+        function error() {
+            toastr.error(`There was an error submitting your e-mail.`);
+        }
+
+        // handle the form submission event
+
+        form.addEventListener("submit", function (ev) {
+            ev.preventDefault();
+            var data = new FormData(form);
+            ajax(form.method, form.action, data, success, error);
+        });
+    });
+
+    // helper function for sending an AJAX request
+
+    function ajax(method, url, data, success, error) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                success(xhr.response, xhr.responseType);
+            } else {
+                error(xhr.status, xhr.response, xhr.responseType);
+            }
+        };
+        xhr.send(data);
+    }
+})();
